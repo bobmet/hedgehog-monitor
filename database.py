c4918779cf1cf67f30b3d92a3c4b93a450bb19fa
bobmet <rpmetak@gmail.com>
2016-08-22 22:27:14 -0400

    Refactored database code into separate file

---
+++ database.py
@@ -0,0 +1,119 @@
+import sqlite3
+import logging
+
+logger = logging.getLogger('hh')
+
+class DatabaseHandler:
+    """
+    Class to wrap the database creation/queries
+    """
+    def __init__(self, database_name='hedgehog.db'):
+        self.db_name = database_name
+
+    def get_data(self, sql):
+        try:
+            conn = sqlite3.connect(self.db_name)
+            c = conn.cursor()
+
+            result = c.execute(sql).fetchone()
+            conn.close()
+
+            data = tuple(result)
+        except sqlite3.OperationalError as ex:
+            logger.error("Database error retrieving data: {0}".format(ex))
+            data = None
+
+        return data
+
+    def get_environment_data(self):
+        """
+        Retrieves the most recent temperature, humidity, and light levels from the database
+        :return:
+        """
+        wx = self.get_last_wx_data()
+        lux = self.get_last_light_data()
+
+        if wx is not None and lux is not None:
+            data = (wx[0], wx[1], lux[0])
+        else:
+            data = None
+
+        return data
+
+    def get_wheel_data(self):
+        """
+        Gets the most recent wheel information:  distance traveled, number of revolutions, and moving time
+        over the last 24 hours
+        :return:
+        """
+        sql = "select sum(distance),sum(revolutions), sum(moving_time) " \
+              "from wheel_tbl where timestamp > datetime('now', '-24 hours')"
+
+        return self.get_data(sql)
+
+    def get_last_light_data(self):
+        sql = "select lux, timestamp from light_tbl order by timestamp DESC"
+        data = self.get_data(sql)
+        return data
+
+    def get_last_wx_data(self):
+        """
+        Retrieves the most recent value from the temperature/humidity table
+        :param self:
+        :return:
+        """
+        sql = "select temp, humidity, timestamp from temp_tbl order by timestamp DESC"
+        data = self.get_data(sql)
+        return data
+
+
+    def create_database(self):
+        """
+        Creates the database and tables
+        :return:
+        """
+        conn = sqlite3.connect(self.db_name)
+
+        c = conn.cursor()
+        c.execute("CREATE TABLE temp_tbl (timestamp DATETIME, temp REAL, humidity REAL, remote INTEGER)")
+        c.execute("CREATE TABLE light_tbl (timestamp DATETIME, lux INTEGER, remote INTEGER)")
+        c.execute("CREATE TABLE wheel_tbl(timestamp DATETIME, revolutions REAL, distance REAL, "
+                  "moving_time REAL, avg_speed REAL, active INTEGER, remote INTEGER)")
+        conn.commit()
+        conn.close()
+
+    def save_to_database(self, data):
+        """
+
+        :param data:
+        :return:
+        """
+        data_type = data['data_type']
+        if data_type == 'temperature':
+            sql = ("INSERT INTO temp_tbl (timestamp, temp, humidity) values ('{0}', {1}, {2})".format(
+                data['datetime'],
+                data['temp_f'],
+                data['humidity']))
+        elif data_type == "wheel":
+            active = 1 if data['active'] is True else 0
+            sql = ("INSERT INTO wheel_tbl (timestamp, revolutions, distance, moving_time, avg_speed, active)"
+                   "values ('{0}', {1}, {2}, {3}, {4}, {5})".format(data['datetime'],
+                                                                    data['revolutions'],
+                                                                    data['distance'],
+                                                                    data['moving_time'],
+                                                                    data['avg_speed'],
+                                                                    active))
+        elif data_type == 'light':
+            sql = ("INSERT INTO light_tbl (timestamp, lux) values ('{0}', {1})".format(data['datetime'],
+                                                                                       data['lux']))
+        else:
+            return
+
+        conn = sqlite3.connect(self.db_name)
+        c = conn.cursor()
+        try:
+            c.execute(sql)
+            conn.commit()
+            conn.close()
+        except Exception as ex:
+            logger.error("Error inserting data into table:  {0}".format(ex))
--- sensor_handlers/lcd_handler.py
+++ sensor_handlers/lcd_handler.py
@@ -4,6 +4,7 @@ import logging
 import datetime
 import sqlite3
 import os
+from database import DatabaseHandler
 
 __version__ = "0.0.7"
 
@@ -39,10 +40,11 @@ class DataReportingThread(threading.Thread):
         self.first_message = 0
         self.timeout_counter = 0
 
-        self.db = DatabaseHandler()
+        db_name = 'hedgehog.db'
+        self.db = DatabaseHandler(db_name)
 
-        if os.path.exists("hedgehog.db") is False:
-            logger.info("Database file {0} not found, creating".format("hedgehog.db"))
+        if os.path.exists(db_name) is False:
+            logger.info("Database file {0} not found, creating".format(db_name))
             self.db.create_database()
 
     def run(self):
@@ -227,110 +229,3 @@ class DataReportingThread(threading.Thread):
         return result
 
 
-class DatabaseHandler:
-    """
-    Class to wrap the database creation/queries
-    """
-    def __init__(self):
-        self.db_name = 'hedgehog.db'
-
-    def get_data(self, sql):
-        try:
-            conn = sqlite3.connect(self.db_name)
-            c = conn.cursor()
-
-            result = c.execute(sql).fetchone()
-            conn.close()
-
-            data = tuple(result)
-        except sqlite3.OperationalError as ex:
-            logger.error("Database error retrieving data: {0}".format(ex))
-            data = None
-
-        return data
-
-    def get_environment_data(self):
-        wx = self.get_last_wx_data()
-        lux = self.get_last_light_data()
-
-        if wx is not None and lux is not None:
-            data = (wx[0], wx[1], lux[0])
-        else:
-            data = None
-
-        return data
-
-    def get_wheel_data(self):
-        sql = "select sum(distance),sum(revolutions), sum(moving_time) " \
-              "from wheel_tbl where timestamp > datetime('now', '-24 hours')"
-
-        return self.get_data(sql)
-
-    def get_last_light_data(self):
-        sql = "select lux, timestamp from light_tbl order by timestamp DESC"
-        data = self.get_data(sql)
-        return data
-
-    def get_last_wx_data(self):
-        """
-        Retrieves the most recent value from the temperature/humidity table
-        :param self:
-        :return:
-        """
-        sql = "select temp, humidity, timestamp from temp_tbl order by timestamp DESC"
-        data = self.get_data(sql)
-        return data
-
-
-    def create_database(self):
-        """
-        Creates the database and tables
-        :return:
-        """
-        conn = sqlite3.connect(self.db_name)
-
-        c = conn.cursor()
-        c.execute("CREATE TABLE temp_tbl (timestamp DATETIME, temp REAL, humidity REAL, remote INTEGER)")
-        c.execute("CREATE TABLE light_tbl (timestamp DATETIME, lux INTEGER, remote INTEGER)")
-        c.execute("CREATE TABLE wheel_tbl(timestamp DATETIME, revolutions REAL, distance REAL, "
-                  "moving_time REAL, avg_speed REAL, active INTEGER, remote INTEGER)")
-        conn.commit()
-        conn.close()
-
-    def save_to_database(self, data):
-        """
-
-        :param data:
-        :return:
-        """
-        data_type = data['data_type']
-        if data_type == 'temperature':
-            sql = ("INSERT INTO temp_tbl (timestamp, temp, humidity) values ('{0}', {1}, {2})".format(
-                data['datetime'],
-                data['temp_f'],
-                data['humidity']))
-        elif data_type == "wheel":
-            active = 1 if data['active'] is True else 0
-            sql = ("INSERT INTO wheel_tbl (timestamp, revolutions, distance, moving_time, avg_speed, active)"
-                   "values ('{0}', {1}, {2}, {3}, {4}, {5})".format(
-                data['datetime'],
-                data['revolutions'],
-                data['distance'],
-                data['moving_time'],
-                data['avg_speed'],
-                active))
-        elif data_type == 'light':
-            sql = ("INSERT INTO light_tbl (timestamp, lux) values ('{0}', {1})".format(
-                data['datetime'],
-                data['lux']))
-        else:
-            return
-
-        conn = sqlite3.connect("hedgehog.db")
-        c = conn.cursor()
-        try:
-            c.execute(sql)
-            conn.commit()
-            conn.close()
-        except Exception as ex:
-            logger.error("Error inserting data into table:  {0}".format(ex))
